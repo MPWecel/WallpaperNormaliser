95f78c455bbc7159702a473fb19e85c432646895
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-04-28 00:36:14 +0200

    Add extension and MIME parsers to FileFormatInfo
    Add FromExtension and FromMimeType factory methods to map file extensions and MIME types to FileFormatInfo (returns null for unknown values). Fix MIME type typo for PNG (image/ong -> image/png) and normalize switch formatting.

--- src/WallpaperNormaliser/WallpaperNormaliser.Core/ValueObjects/FileFormatInfo.cs
+++ src/WallpaperNormaliser/WallpaperNormaliser.Core/ValueObjects/FileFormatInfo.cs
@@ -1,4 +1,5 @@
-﻿using WallpaperNormaliser.Core.Enums;
+﻿using System.Net.Http.Headers;
+using WallpaperNormaliser.Core.Enums;
 
 namespace WallpaperNormaliser.Core.ValueObjects;
 public sealed record FileFormatInfo(EFileFormat Format)
@@ -6,24 +7,52 @@ public sealed record FileFormatInfo(EFileFormat Format)
     public string Extension 
         => Format switch
         {
-            EFileFormat.Jpeg    =>  "jpg",
-            EFileFormat.Png     =>  "png",
-            EFileFormat.Bmp     =>  "bmp",
-            EFileFormat.Gif     =>  "gif",
-            EFileFormat.Tiff    =>  "tiff",
-            EFileFormat.Webp    =>  "webp",
+            EFileFormat.Jpeg => "jpg",
+            EFileFormat.Png  => "png",
+            EFileFormat.Bmp  => "bmp",
+            EFileFormat.Gif  => "gif",
+            EFileFormat.Tiff => "tiff",
+            EFileFormat.Webp => "webp",
             _ => "bin"
         };
 
     public string MimeType
         => Format switch
         {
-            EFileFormat.Jpeg    =>  "image/jpeg",
-            EFileFormat.Png     =>  "image/ong",
-            EFileFormat.Bmp     =>  "image/bmp",
-            EFileFormat.Gif     =>  "image/gif",
-            EFileFormat.Tiff    =>  "image/tiff",
-            EFileFormat.Webp    =>  "image/webp",
+            EFileFormat.Jpeg => "image/jpeg",
+            EFileFormat.Png  => "image/png",
+            EFileFormat.Bmp  => "image/bmp",
+            EFileFormat.Gif  => "image/gif",
+            EFileFormat.Tiff => "image/tiff",
+            EFileFormat.Webp => "image/webp",
             _ => "application/octet-stream"
+        };
+
+    public static FileFormatInfo? FromExtension(string extension)
+        => extension.ToLowerInvariant() switch
+        {
+            "jpg"  => new FileFormatInfo(EFileFormat.Jpeg),
+            "jpeg" => new FileFormatInfo(EFileFormat.Jpeg),
+            "gif"  => new FileFormatInfo(EFileFormat.Gif),
+            "bmp"  => new FileFormatInfo(EFileFormat.Bmp),
+            "png"  => new FileFormatInfo(EFileFormat.Png),
+            "tif"  => new FileFormatInfo(EFileFormat.Tiff),
+            "tiff" => new FileFormatInfo(EFileFormat.Tiff),
+            "webp" => new FileFormatInfo(EFileFormat.Webp),
+            _ => (FileFormatInfo?)(null)
+        };
+
+    public static FileFormatInfo? FromMimeType(string mimeType)
+        => mimeType.ToLowerInvariant() switch
+        {
+            "image/jpg"  => new FileFormatInfo(EFileFormat.Jpeg),
+            "image/jpeg" => new FileFormatInfo(EFileFormat.Jpeg),
+            "image/gif"  => new FileFormatInfo(EFileFormat.Gif),
+            "image/bmp"  => new FileFormatInfo(EFileFormat.Bmp),
+            "image/png"  => new FileFormatInfo(EFileFormat.Png),
+            "image/tif"  => new FileFormatInfo(EFileFormat.Tiff),
+            "image/tiff" => new FileFormatInfo(EFileFormat.Tiff),
+            "image/webp" => new FileFormatInfo(EFileFormat.Webp),
+            _ => (FileFormatInfo?)(null)
         };
 }
