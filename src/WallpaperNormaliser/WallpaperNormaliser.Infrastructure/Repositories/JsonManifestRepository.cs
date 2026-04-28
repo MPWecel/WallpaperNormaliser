21068100f3447b53a292d6323eb844123ef9dd92
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-04-28 13:31:14 +0200

    Beginning of implementation of Infrastructure layer
    As in title

--- src/WallpaperNormaliser/WallpaperNormaliser.Infrastructure/Repositories/JsonManifestRepository.cs
+++ src/WallpaperNormaliser/WallpaperNormaliser.Infrastructure/Repositories/JsonManifestRepository.cs
@@ -4,9 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using WallpaperNormaliser.Core.
+using WallpaperNormaliser.Core.Contracts;
+using WallpaperNormaliser.Core.Models.Manifest;
 
 namespace WallpaperNormaliser.Infrastructure.Repositories;
 public class JsonManifestRepository : IManifestRepository
 {
+    public Task<ManifestDocument?> GetByHashAsync(string hash, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ManifestDocument?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ManifestDocument?> GetByNameAsync(string fileName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<ManifestDocument>> GetManyAsync(ManifestQuery query, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveAsync(ManifestDocument document, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
 }
