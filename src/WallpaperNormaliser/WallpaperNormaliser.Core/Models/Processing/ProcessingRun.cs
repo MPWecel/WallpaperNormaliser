9a01023a16059151dc9457020c0d7479490071e1
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-01 23:54:44 +0200

    Update ProcessingRun.cs
    Completed FromString factory method.

--- src/WallpaperNormaliser/WallpaperNormaliser.Core/Models/Processing/ProcessingRun.cs
+++ src/WallpaperNormaliser/WallpaperNormaliser.Core/Models/Processing/ProcessingRun.cs
@@ -39,10 +39,59 @@ public sealed record ProcessingRun(
                                                         .ToDictionary(x=>x.Key, x => x.Value);
 
         const string runIdKey = "RunId";
-        string RunIdValue = entries[runIdKey];
-        bool parseResult = entries.TryGetValue(runIdKey, out string runId);
+        bool parseResult = entries.TryGetValue(runIdKey, out string runIdValue);
+        const string startedUtcKey = "StartedUtc";
+        parseResult &= entries.TryGetValue(startedUtcKey, out string startedTimestampString);
+        parseResult &= DateTime.TryParse(startedTimestampString, out DateTime startedUtcValue);
+        
+        const string finishedUtcKey = "FinishedUtc";
+        parseResult &= entries.TryGetValue(finishedUtcKey, out string finishedTimestampString);
+        DateTime? finishedUtcNullableValue;
+        
+        if(String.IsNullOrEmpty(finishedUtcKey) || finishedUtcKey!.Equals("null"))
+        {
+            finishedUtcNullableValue = (DateTime?)(null); 
+        }
+        else
+        {
+            parseResult &= DateTime.TryParse(finishedTimestampString, out DateTime finishedUtcValue);
+            finishedUtcNullableValue = (DateTime?)(finishedUtcValue);
+        }
+        
+        const string StatusKey = "Status";
+        parseResult &= entries.TryGetValue(StatusKey, out string statusValueString);
+        parseResult &= Int32.TryParse(statusValueString, out int statusValueInt);
+        EProcessingStatus statueValueEnum = (EProcessingStatus)(statusValueInt);
+
+        const string TotalFilesKey = "TotalFiles";
+        parseResult &= entries.TryGetValue(TotalFilesKey, out string TotalFilesValueString);
+        parseResult &= Int32.TryParse(TotalFilesValueString, out int TotalFilesValueInt);
+
+        const string successCountKey = "SuccessCount";
+        parseResult &= entries.TryGetValue(successCountKey, out string successCountValueString);
+        parseResult &= Int32.TryParse(successCountValueString, out int successCountValueInt);
+
+        const string failedCountKey = "FailedCount";
+        parseResult &= entries.TryGetValue(failedCountKey, out string failedCountValueString);
+        parseResult &= Int32.TryParse(failedCountValueString, out int failedCountValueInt);
+
+        const string skippedCountKey = "SkippedCount";
+        parseResult &= entries.TryGetValue(skippedCountKey, out string skippedCountValueString);
+        parseResult &= Int32.TryParse(skippedCountValueString, out int skippedCountValueInt);
+
+        if (!parseResult)
+            return null;
 
 
-        return new ProcessingRun(RunIdValue)
+        return new ProcessingRun(
+                                    runIdValue,
+                                    startedUtcValue,
+                                    finishedUtcNullableValue,
+                                    statueValueEnum,
+                                    TotalFilesValueInt,
+                                    successCountValueInt,
+                                    failedCountValueInt,
+                                    skippedCountValueInt
+                                );
     }
 }
