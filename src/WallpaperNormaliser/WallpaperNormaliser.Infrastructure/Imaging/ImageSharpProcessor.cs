21e378e0a2ab9c42c136068bc1617f50485f9f85
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-09 08:03:41 +0200

    Update ImageSharpProcessor.cs

--- src/WallpaperNormaliser/WallpaperNormaliser.Infrastructure/Imaging/ImageSharpProcessor.cs
+++ src/WallpaperNormaliser/WallpaperNormaliser.Infrastructure/Imaging/ImageSharpProcessor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using SixLabors;
+﻿using SixLabors;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Jpeg;
@@ -19,25 +13,23 @@ using WallpaperNormaliser.Core.Models.Processing;
 namespace WallpaperNormaliser.Infrastructure.Imaging;
 public class ImageSharpProcessor : IImageProcessor
 {
-    public async Task<ImageProcessingResult> ProcessAsync(
-                                                    FileContext input, 
-                                                    ProcessingOptions options, 
-                                                    CancellationToken cancellationToken = default
-                                                   )
+    public async Task<ImageProcessingResult> ProcessAsync(FileContext input, ProcessingOptions options, CancellationToken cancellationToken = default)
     {
+        DateTime startDateUtc = DateTime.UtcNow;
         using Stream byteStream = new MemoryStream(input.Bytes, writable: false);
-        using Image image = await Image.LoadAsync(byteStream, cancellationToken);
+        using Image image = await Image.LoadAsync<Rgba32>(byteStream, cancellationToken);
+
+        string? validationMessage = ValidateMinimumSize(image.Width, image.Height);
 
         image.Mutate(x => x.AutoOrient());
 
-        double[] scaleAvailableValues =
-                                        [
+        double[] scaleAvailableValues = [
                                             1.0,
                                             ((double)(options.TargetResolution.Width / image.Width)),
                                             ((double)(options.TargetResolution.Height / image.Height))
                                         ];
 
-        double scale = scaleAvailableValues.Min();
+        double scale = GetMinValue(scaleAvailableValues);
 
         int newWidth = Math.Max(1, ((int)(Math.Round(image.Width * scale))));
         int newHeight = Math.Max(1, ((int)(Math.Round(image.Height * scale))));
@@ -58,20 +50,26 @@ public class ImageSharpProcessor : IImageProcessor
         using MemoryStream saveStream = new();
 
         await canvas.SaveAsJpegAsync(saveStream, new JpegEncoder() { Quality = options.JpegQuality }, cancellationToken);
+        DateTime finishedUtc = DateTime.UtcNow;
+        TimeSpan duration = (finishedUtc - startDateUtc);
 
         ImageProcessingResult result = new(
                                             EProcessingStatus.Completed, 
                                             saveStream.ToArray(), 
-                                            new FileFormatInfo(EFileFormat.Jpeg), 
-                                            (int)(options.TargetResolution.Width), 
-                                            (int)(options.TargetResolution.Height), 
+                                            new FileFormatInfo(EFileFormat.Jpeg),
+                                            new Resolution(options.TargetResolution.Width, options.TargetResolution.Height), 
+                                            validationMessage, 
                                             null, 
-                                            null, 
-                                            TimeSpan.Zero
+                                            duration
                                           );
 
         return result;
     }
 
     private double GetMinValue(IEnumerable<double> input) => input.Min();
+
+    private string? ValidateMinimumSize(int width, int height)
+        => (width < 640 || height < 480) ?
+                                         ($"Image resolution may be too low!\tInput resolution: {width}x{height}.\tRecommended minimum resolution: 640x480") :
+                                         (null);
 }
