d6df6666f6bc59a14e4e4c8677a3160c8f5fa0b3
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-09 07:35:20 +0200

    Update SqliteRunRepository.cs

--- src/WallpaperNormaliser/WallpaperNormaliser.Infrastructure/Persistence/Repositories/SqliteRunRepository.cs
+++ src/WallpaperNormaliser/WallpaperNormaliser.Infrastructure/Persistence/Repositories/SqliteRunRepository.cs
@@ -1,13 +1,8 @@
-﻿using Dapper;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+
+using Dapper;
 
 using WallpaperNormaliser.Core.Contracts;
-using WallpaperNormaliser.Core.Models;
 using WallpaperNormaliser.Core.Models.Processing;
 using WallpaperNormaliser.Infrastructure.Persistence.Database;
 
@@ -18,81 +13,6 @@ public sealed class SqliteRunRepository : IRunRepository
     private readonly SqliteConnectionFactory _connectionFactory;
 
     public SqliteRunRepository(SqliteConnectionFactory connectionFactory) => _connectionFactory = connectionFactory;
-
-    public async Task CreateRunAsync(ProcessingRun run, CancellationToken cancellationToken = default)
-    {
-        using IDbConnection db = _connectionFactory.Create();
-        const string insertScript = """
-                                       INSERT INTO [ProcessingRuns] ([RunId], [StartedUtc], [FinishedUtc], [Status], [TotalFiles], [SuccessCount], [FailedCount], [SkippedCount])
-                                       VALUES (@RunId, @StartedUtc, @FinishedUtc, @TotalFiles, @SuccessCount, @FailedCount, @SkippedCount)
-                                    """;
-
-        await db.ExecuteAsync(insertScript, run);
-    }
-
-    public async Task AddRunItemAsync(ProcessingRunItem item, CancellationToken cancellationToken = default)
-    {
-        using IDbConnection db = _connectionFactory.Create();
-        const string insertScript = """
-                                       INSERT INTO [ProcessingRunItems] ([Id], [RunId], [SourceHash], [FileName], [Status], [Message], [DurationMs], [CreatedUtc])
-                                       VALUES (@Id, @RunId, @SourceHash, @FileName, @Status, @Message, @DurationMs, @CreatedUtc)
-                                    """;
-
-        await db.ExecuteAsync(insertScript, item);
-    }
-
-    public async Task UpsertRunAsync(ProcessingRun run, CancellationToken cancellationToken = default)
-    {
-        using IDbConnection db = _connectionFactory.Create();
-        const string selectScript = """
-                                       SELECT [RunId], [StartedUtc], [FinishedUtc], [Status], [TotalFiles], [SuccessCount], [FailedCount], [SkippedCount]
-                                       FROM [ProcessingRuns]
-                                       WHERE [RunId] = @RunId
-                                    """;
-        ProcessingRun? fromDb = await db.QuerySingleOrDefaultAsync<ProcessingRun>(selectScript, new { run.Id });
-        bool isRunInDb = (fromDb is not null);
-
-        if (isRunInDb)
-            await UpdateRunAsync(db, run, fromDb!, cancellationToken).ConfigureAwait(false);
-        else
-            await CreateRunAsync(run, cancellationToken).ConfigureAwait(false);
-    }
-
-    private async Task UpdateRunAsync(IDbConnection db, ProcessingRun run, ProcessingRun fromDb, CancellationToken cancellationToken)
-    {
-        List<string> setBuilderList = new(8);
-        //[ProcessingRuns] ([Id], [StartedUtc], [TargetWidth], [TargetHeight], [Quality])
-        if (run.Status != fromDb.Status)
-            setBuilderList.Add($"[Status]={run.Status}");
-
-        if (run.FinishedUtc != fromDb.FinishedUtc)
-            setBuilderList.Add($"[FinishedUtc]={run.FinishedUtc?.ToString("yyyy-MM-dd HH:mm:ss.ff") ?? "null"}");
-
-        if (run.TotalFiles != fromDb.TotalFiles)
-            setBuilderList.Add($"[TotalFiles]={run.TotalFiles}");
-
-        if (run.SuccessCount != fromDb.SuccessCount)
-            setBuilderList.Add($"[SuccessCount]={run.SuccessCount}");
-
-        if (run.FailedCount != fromDb.FailedCount)
-            setBuilderList.Add($"[FailedCount]={run.FailedCount}");
-
-        if (run.SkippedCount != fromDb.SkippedCount)
-            setBuilderList.Add($"[SkippedCount]={run.SkippedCount}");
-
-        string setInstruction = String.Join(", ", setBuilderList);
-
-        string updateScript = $"""
-                                 UPDATE [ProcessingRunItems]
-                                 SET {setInstruction}
-                                 WHERE [Id] = {run.Id}
-                              """;
-
-        await db.ExecuteAsync(updateScript);
-    }
-
-    public async Task FinaliseRunAsync(ProcessingRun run, CancellationToken cancellationToken = default)
-        => await UpsertRunAsync(run, cancellationToken);
 
     public async Task<ProcessingRun?> GetRunAsync(string runId, CancellationToken cancellationToken = default)
     {
@@ -134,4 +54,77 @@ public sealed class SqliteRunRepository : IRunRepository
         List<ProcessingRunItem> result = rows?.ToList() ?? new();
         return result;
     }
+
+    public async Task CreateRunAsync(ProcessingRun run, CancellationToken cancellationToken = default)
+    {
+        using IDbConnection db = _connectionFactory.Create();
+        const string insertScript = """
+                                       INSERT INTO [ProcessingRuns] ([RunId], [StartedUtc], [FinishedUtc], [Status], [TotalFiles], [SuccessCount], [FailedCount], [SkippedCount])
+                                       VALUES (@RunId, @StartedUtc, @FinishedUtc, @TotalFiles, @SuccessCount, @FailedCount, @SkippedCount)
+                                    """;
+        await db.ExecuteAsync(insertScript, run);
+    }
+
+    public async Task AddRunItemAsync(ProcessingRunItem item, CancellationToken cancellationToken = default)
+    {
+        using IDbConnection db = _connectionFactory.Create();
+        const string insertScript = """
+                                       INSERT INTO [ProcessingRunItems] ([Id], [RunId], [SourceHash], [FileName], [Status], [Message], [DurationMs], [CreatedUtc])
+                                       VALUES (@Id, @RunId, @SourceHash, @FileName, @Status, @Message, @DurationMs, @CreatedUtc)
+                                    """;
+        await db.ExecuteAsync(insertScript, item);
+    }
+
+    public async Task UpsertRunAsync(ProcessingRun run, CancellationToken cancellationToken = default)
+    {
+        using IDbConnection db = _connectionFactory.Create();
+        const string selectScript = """
+                                       SELECT [RunId], [StartedUtc], [FinishedUtc], [Status], [TotalFiles], [SuccessCount], [FailedCount], [SkippedCount]
+                                       FROM [ProcessingRuns]
+                                       WHERE [RunId] = @RunId
+                                    """;
+        ProcessingRun? fromDb = await db.QuerySingleOrDefaultAsync<ProcessingRun>(selectScript, new { run.Id });
+        bool isRunInDb = (fromDb is not null);
+
+        if (isRunInDb)
+            await UpdateRunAsync(db, run, fromDb!, cancellationToken).ConfigureAwait(false);
+        else
+            await CreateRunAsync(run, cancellationToken).ConfigureAwait(false);
+    }
+
+    private async Task UpdateRunAsync(IDbConnection db, ProcessingRun run, ProcessingRun fromDb, CancellationToken cancellationToken)
+    {
+        List<string> setBuilderList = new(8);
+
+        if (run.Status != fromDb.Status)
+            setBuilderList.Add($"[Status]={run.Status}");
+
+        if (run.FinishedUtc != fromDb.FinishedUtc)
+            setBuilderList.Add($"[FinishedUtc]={run.FinishedUtc?.ToString("yyyy-MM-dd HH:mm:ss.ff") ?? "null"}");
+
+        if (run.TotalFiles != fromDb.TotalFiles)
+            setBuilderList.Add($"[TotalFiles]={run.TotalFiles}");
+
+        if (run.SuccessCount != fromDb.SuccessCount)
+            setBuilderList.Add($"[SuccessCount]={run.SuccessCount}");
+
+        if (run.FailedCount != fromDb.FailedCount)
+            setBuilderList.Add($"[FailedCount]={run.FailedCount}");
+
+        if (run.SkippedCount != fromDb.SkippedCount)
+            setBuilderList.Add($"[SkippedCount]={run.SkippedCount}");
+
+        string setInstruction = String.Join(", ", setBuilderList);
+
+        string updateScript = $"""
+                                 UPDATE [ProcessingRunItems]
+                                 SET {setInstruction}
+                                 WHERE [Id] = {run.Id}
+                              """;
+
+        await db.ExecuteAsync(updateScript);
+    }
+
+    public async Task FinaliseRunAsync(ProcessingRun run, CancellationToken cancellationToken = default)
+        => await UpsertRunAsync(run, cancellationToken);
 }
