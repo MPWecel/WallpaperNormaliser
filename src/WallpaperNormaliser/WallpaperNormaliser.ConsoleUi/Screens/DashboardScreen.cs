0d7d70774dfedc90bc51b0595c5e1bf6807cc8b2
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-10 23:04:40 +0200

    Create DashboardScreen.cs

---
+++ src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/Screens/DashboardScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Spectre.Console;
+
+using WallpaperNormaliser.ConsoleUi.Services;
+
+namespace WallpaperNormaliser.ConsoleUi.Screens;
+public sealed class DashboardScreen
+{
+    private readonly WorkingDirectoryResolver _paths;
+
+    public DashboardScreen(WorkingDirectoryResolver resolver)
+    {
+        _paths = resolver;
+    }
+
+    public async Task ShowAsync()
+    {
+        AnsiConsole.Clear();
+        var root = _paths.GetRoot();
+        Table table = new();
+
+        AnsiConsole.Write(table);
+        Console.ReadKey(true);
+
+        return Task.CompletedTask;
+    }
+}
