10202ee0e5e9128cbf51236cec1f2f1005547e02
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-10 23:04:23 +0200

    Create WorkingDirectoryResolver.cs

---
+++ src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/Services/WorkingDirectoryResolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace WallpaperNormaliser.ConsoleUi.Services;
+public sealed class WorkingDirectoryResolver
+{
+    public string GetRoot() => Path.Combine(AppContext.BaseDirectory, "APPLICATION_WORKING_DIRECTORY");
+}
