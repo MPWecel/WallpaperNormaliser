b11d40b04912192f1e44c34e194f4703227b9f5e
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-10 23:06:10 +0200

    Create SettingsValidator.cs
    Stub

---
+++ src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/Services/SettingsValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallpaperNormaliser.ConsoleUi.Services;
+public sealed class SettingsValidator
+{
+    public bool Validate() => true;
+}
