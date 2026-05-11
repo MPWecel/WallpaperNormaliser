ca1a709cec71b3bfb331ee0f390dbaf8a116534b
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-11 16:40:09 +0200

    Update SettingsScreen.cs
    Refactor using established menu pattern

--- src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/Screens/SettingsScreen.cs
+++ src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/Screens/SettingsScreen.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Spectre.Console;
+﻿using Spectre.Console;
 
 using WallpaperNormaliser.Core.Contracts;
 using WallpaperNormaliser.Core.Models.Common;
@@ -12,9 +6,21 @@ using WallpaperNormaliser.Core.Models.Settings;
 
 
 namespace WallpaperNormaliser.ConsoleUi.Screens;
-public sealed class SettingsScreen(ISettingsRepository settingsRepository)
+public sealed class SettingsScreen
 {
-    private readonly ISettingsRepository _settingsRepository = settingsRepository;
+    private readonly ISettingsRepository _settingsRepository;
+
+    private readonly IReadOnlyDictionary<string, Func<Task>> _menuActionMappings;
+
+    public SettingsScreen(ISettingsRepository settingsRepository)
+    {
+        _settingsRepository = settingsRepository;
+        _menuActionMappings = new Dictionary<string, Func<Task>>()
+        {
+            [SettingsScreenConstants.ChangeResolution] = ChangeResolutionAsync,
+            [SettingsScreenConstants.ChangeJpegQuality] = ChangeJpegQualityAsync
+        };
+    }
 
     public async Task ShowAsync()
     {
@@ -24,44 +30,92 @@ public sealed class SettingsScreen(ISettingsRepository settingsRepository)
 
             AppSettings settings = await _settingsRepository.GetAsync();
 
-            Resolution resolution = settings.Resolution;
+            RenderSettingsTable(settings);
 
-            Table table = new();
-            table.AddColumn("SETTING")
-                 .AddColumn("VALUE");
+            SelectionPrompt<string> prompt = new();
+            prompt.Title(SettingsScreenConstants.SettingsScreenMenuPrompt)
+                  .AddChoices(SettingsScreenConstants.Choices);
 
-            table.AddRow("Resolution", settings.Resolution.ToString())
-                 .AddRow("Quality", settings.Quality.ToString());
+            string choice = AnsiConsole.Prompt(prompt);
 
-            AnsiConsole.Write(table);
+            if (choice.Equals(SettingsScreenConstants.Back, StringComparison.OrdinalIgnoreCase))
+                return;
 
-            string choice = AnsiConsole.Prompt(
-                                                  new SelectionPrompt<string>().Title("")
-                                                                               .AddChoices(
-                                                                                              "[1] Change resolution",
-                                                                                              "[2] Change quality",
-                                                                                              "[3] Back."
-                                                                                          )
-                                              );
+            bool isChoiceActionDefined = _menuActionMappings.TryGetValue(choice, out Func<Task>? action);
 
-            switch(choice)
-            {
-                case "Change resolution":
-                    uint width = AnsiConsole.Ask<uint>("Target width: ", resolution.Width);
-                    uint height = AnsiConsole.Ask<uint>("Target height: ", resolution.Height);
+            if (!isChoiceActionDefined)
+                continue;
 
-                    settings = settings with { Resolution = new(width, height) };
-                    await _settingsRepository.SaveAsync(settings);
-                    AnsiConsole.MarkupLine($"[green]Saved resolution {settings.Resolution.ToString()}[/]");
-                    break;
-                case "Change quality":
-                    break;
-                case "Back":
-                    return;
-                default:
-                    return;
-            }
-
+            await action!();
         }
     }
+
+    private async Task ChangeResolutionAsync()
+    {
+        AppSettings settings = await _settingsRepository.GetAsync();
+
+        uint width = AnsiConsole.Ask<uint>(SettingsScreenConstants.ChangeResolutionPromptWidth, settings.Resolution.Width);
+        uint height = AnsiConsole.Ask<uint>(SettingsScreenConstants.ChangeResolutionPromptHeight, settings.Resolution.Height);
+        AppSettings updated = settings with { Resolution = new Resolution(width, height) };
+        await _settingsRepository.SaveAsync(updated);
+
+        AnsiConsole.MarkupLine($"[green]{SettingsScreenConstants.ChangeResolutionSuccessMessage}{updated.Resolution.ToString()}[/]");
+        AnsiConsole.Console.Input.ReadKey(false);
+    }
+
+    private async Task ChangeJpegQualityAsync()
+    {
+        AppSettings settings = await _settingsRepository.GetAsync();
+
+        int quality = AnsiConsole.Ask<int>(SettingsScreenConstants.ChangeQualityPrompt, settings.Quality);
+        AppSettings updated = settings with { Quality = quality };
+        await _settingsRepository.SaveAsync(updated);
+
+        AnsiConsole.MarkupLine($"[green]{SettingsScreenConstants.ChangeQualitySuccessMessage}{quality}[/]");
+        AnsiConsole.Console.Input.ReadKey(false);
+    }
+
+    private static void RenderSettingsTable(AppSettings settings)
+    {
+        Table table = new();
+
+        table.AddColumn(SettingsScreenConstants.Setting)
+             .AddColumn(SettingsScreenConstants.Value);
+
+        table.AddRow(SettingsScreenConstants.TableRowLabel_Resolution, settings.Resolution.ToString())
+             .AddRow(SettingsScreenConstants.TableRowLabel_Quality, settings.Quality.ToString());
+
+        AnsiConsole.Write(table);
+    }
+}
+
+internal static class SettingsScreenConstants
+{
+    internal const string Title = "SETTINGS";
+
+    internal const string SettingsScreenMenuPrompt = "Choose your action:\t";
+
+    internal const string Setting = "SETTING";
+    internal const string Value = "VALUE";
+
+    internal const string ChangeResolutionPromptWidth = "Target width:\t";
+    internal const string ChangeResolutionPromptHeight = "Target height:\t";
+    internal const string ChangeResolutionSuccessMessage = "Saved resolution:\t";
+
+    internal const string ChangeQualityPrompt = "New JPEG quality:\t";
+    internal const string ChangeQualitySuccessMessage = "Saved JPEG quality:\t";
+
+
+    internal const string TableRowLabel_Resolution = "Resolution";
+    internal const string TableRowLabel_Quality = "Quality";
+
+    internal const string ChangeResolution = "[1] Change resolution";
+    internal const string ChangeJpegQuality = "[2] Change JPEG quality";
+    internal const string Back = "[0] Back";
+
+    internal static readonly string[] Choices = [
+                                                    ChangeResolution,
+                                                    ChangeJpegQuality,
+                                                    Back
+                                                ];
 }
