f4d6daba9f26a72c3837094a4bd71782ace1bd15
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-12 04:54:08 +0200

    Application Service for Settings Screen - contract and implementation

---
+++ src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/ApplicationServices/Interfaces/ISettingsApplicationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WallpaperNormaliser.ConsoleUi.Models;
+using WallpaperNormaliser.ConsoleUi.Models.ViewModels;
+
+namespace WallpaperNormaliser.ConsoleUi.ApplicationServices.Interfaces;
+public interface ISettingsApplicationService
+{
+    Task<SettingsViewModel> GetSettingsAsync();
+    Task SaveSettingsAsync(SettingsViewModel toSave);
+}
---
+++ src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/ApplicationServices/SettingsApplicationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WallpaperNormaliser.ConsoleUi.ApplicationServices.Interfaces;
+using WallpaperNormaliser.ConsoleUi.Models.ViewModels;
+using WallpaperNormaliser.Core.Contracts;
+using WallpaperNormaliser.Core.Models.Settings;
+
+namespace WallpaperNormaliser.ConsoleUi.ApplicationServices;
+public sealed class SettingsApplicationService(ISettingsRepository settingsRepository) : ISettingsApplicationService
+{
+    private readonly ISettingsRepository _repository = settingsRepository;
+
+    public async Task<SettingsViewModel> GetSettingsAsync()
+    {
+        AppSettings domainModel = await GetDomainAppSettingsAsync();
+        SettingsViewModel result = SettingsViewModel.FromDomainEntity(domainModel);
+        return result;
+    }
+
+    public async Task SaveSettingsAsync(SettingsViewModel toSave)
+    {
+        AppSettings fromSource = await GetDomainAppSettingsAsync();
+        AppSettings toSource = toSave.ToDomainEntity(fromSource);
+        await _repository.SaveAsync(toSource);
+    }
+
+    private async Task<AppSettings> GetDomainAppSettingsAsync() => await _repository.GetAsync().ConfigureAwait(false);
+}
