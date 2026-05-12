7b5b25899778549fa9655881dc99e98fbba8db44
MPWecel <94813271+MPWecel@users.noreply.github.com>
2026-05-12 04:50:39 +0200

    Slight upgrade of renderer
    Added TableBuilder, modified Theme

---
+++ src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/Rendering/TableBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace WallpaperNormaliser.ConsoleUi.Rendering;
+public static class TableBuilder
+{
+    public static Table Create(params string[] columns)
+    {
+        Table table = new();
+
+        foreach(var column in columns)
+        {
+            table.AddColumn(column);
+        }
+
+        return table;
+    }
+}
--- src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/Rendering/Theme.cs
+++ src/WallpaperNormaliser/WallpaperNormaliser.ConsoleUi/Rendering/Theme.cs
@@ -3,7 +3,7 @@ public static class Theme
 {
     public const string HeaderStyle = "grey";
     public const string SuccessStyle = "green";
-    public const string WarningStyle = "yellow";
+    public const string WarningStyle = "orange";
     public const string ErrorStyle = "red";
     public const string MutedStyle = "silver";
 }
